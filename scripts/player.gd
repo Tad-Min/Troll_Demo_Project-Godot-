75e1074bc968dd5bc836a69dc59e66130dc813d2
VolDeBug <hiepvl987654321@gmail.com>
2025-09-10 11:03:22 +0700

    Task-1
    animation+movement+doublejump+ jumpsound

---
+++ scripts/player.gd
@@ -0,0 +1,77 @@
+extends CharacterBody2D
+
+
+const SPEED = 100.0
+const JUMP_VELOCITY = -200.0
+const GRAVITY : int = 4200
+var last_direction = 0
+var lr_anim : bool = true
+
+const MAX_AIR_JUMPS = 1   # only 1 allowed in air
+var air_jump_done = 0
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	else:
+		air_jump_done = 0
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump"):
+		print("jumping")
+		if is_on_floor():
+			if last_direction == -1.0:
+				lr_anim=false
+				$AnimatedSprite2D.play("jump_left")
+			else:
+				lr_anim=false
+				$AnimatedSprite2D.play("jump_right")
+		else:
+			if air_jump_done==0:
+				velocity.y = JUMP_VELOCITY
+				air_jump_done+=1
+				$JumpSound.play()
+			
+
+
+
+	
+	# Handle movement A-D
+	var direction := Input.get_axis("move_left", "move_right")
+	print(direction)
+	if last_direction != direction and lr_anim:
+		if direction == -1:
+			$AnimatedSprite2D.play("left")
+		if direction == 1:
+			$AnimatedSprite2D.play("right")
+		if direction ==0:
+			$AnimatedSprite2D.play("idle")	
+	last_direction=direction
+	
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	#animation reset after jump
+	if $AnimatedSprite2D.animation== "jump_right" and $AnimatedSprite2D.frame == 9:
+		last_direction = null
+		lr_anim=true
+	if $AnimatedSprite2D.animation== "jump_left" and $AnimatedSprite2D.frame == 9:
+		last_direction = null	
+		lr_anim=true
+
+	move_and_slide()
+
+
+func _on_animated_sprite_2d_frame_changed():
+	if $AnimatedSprite2D.animation == "jump_right":
+		if $AnimatedSprite2D.frame == 4:
+			velocity.y = JUMP_VELOCITY
+			$JumpSound.play()
+	if $AnimatedSprite2D.animation == "jump_left":
+		if $AnimatedSprite2D.frame == 4:
+			velocity.y = JUMP_VELOCITY
+			$JumpSound.play()
