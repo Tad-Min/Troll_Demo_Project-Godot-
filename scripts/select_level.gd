410463acab39f88cf84eb54987b05d2c8b876e5c
vanphuc1704 <kingbach9961@gmail.com>
2025-09-17 14:38:04 +0700

    UI update select lv, pause, next screen

--- scripts/select_level.gd
+++ scripts/select_level.gd
@@ -6,7 +6,11 @@ extends Control
 @onready var btn_lv5: Button = $btnlv5
 @onready var btn_lv6: Button = $btnlv6
 @onready var btn_lv7: Button = $btnlv7
+@onready var btn_lv8: Button = $btnlv8
+@onready var btn_lv9: Button = $btnlv9
 @onready var btn_lv10: Button = $btnlv10
+@onready var btn_lv11: Button = $btnlv11
+@onready var btn_next: Button = $btnNext
 
 func _ready():
 	btn_lv1.pressed.connect(_on_btn_lv1_pressed)
@@ -16,7 +20,11 @@ func _ready():
 	btn_lv5.pressed.connect(_on_btn_lv5_pressed)
 	btn_lv6.pressed.connect(_on_btn_lv6_pressed)
 	btn_lv7.pressed.connect(_on_btn_lv7_pressed)
+	btn_lv8.pressed.connect(_on_btn_lv8_pressed)
+	btn_lv9.pressed.connect(_on_btn_lv9_pressed)
 	btn_lv10.pressed.connect(_on_btn_lv10_pressed)
+	btn_lv11.pressed.connect(_on_btn_lv11_pressed)
+	btn_next.pressed.connect(_on_btn_next_pressed)
 func _on_btn_lv1_pressed():
 	get_tree().change_scene_to_file("res://scenes/Level/Lv1.tscn")
 
@@ -38,5 +46,17 @@ func _on_btn_lv6_pressed():
 func _on_btn_lv7_pressed():
 	get_tree().change_scene_to_file("res://scenes/Level/Lv7.tscn")
 	
+func _on_btn_lv8_pressed():
+	get_tree().change_scene_to_file("res://scenes/Level/Lv8.tscn")
+	
+func _on_btn_lv9_pressed():
+	get_tree().change_scene_to_file("res://scenes/Level/Lv9.tscn")
+	
 func _on_btn_lv10_pressed():
 	get_tree().change_scene_to_file("res://scenes/Level/Lv10.tscn")
+	
+func _on_btn_lv11_pressed():
+	get_tree().change_scene_to_file("res://scenes/Level/Lv11.tscn")
+	
+func _on_btn_next_pressed():
+	get_tree().change_scene_to_file("res://scenes/SelectLevel2.tscn")
