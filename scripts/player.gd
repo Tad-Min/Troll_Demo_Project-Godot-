cc9889c31cdf7faf13822efd8d4d15ac6deea2d8
VolDeBug <hiepvl987654321@gmail.com>
2025-10-06 20:16:55 +0700

    fix player + add lvl18

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,22 +1,23 @@
 extends CharacterBody2D
 
+#signling
 signal died(cause: String)
 
+#adjustable section
 @export var speed: float = 100.0
 @export var jump_velocity: float = 400.0
 @export var gravity_inverted: bool = false
-var min_jc = 0.75
-var max_jc = 1.25
+@export var min_jc = 0.75
+@export var max_jc = 1.25
+@export var MAX_AIR_JUMPS = 1   # only 1 allowed in air by default
+
+#event handling variables
 var jump_charge = min_jc
 const GRAVITY : int = 4200
 var is_on_jumper : bool = false
 var last_direction = 0
-var lr_anim : bool = true
-
 var is_dead: bool = false
-const MAX_AIR_JUMPS = 1   # only 1 allowed in air
 var air_jump_done = 0
-
 var stopmove: bool = false;
 
 func get_jump_velocity() -> float:
@@ -47,34 +48,32 @@ func _physics_process(delta: float) -> void:
 		air_jump_done = 0
 
 	# Handle jump.
-	if not is_on_jumper and Input.is_action_pressed("jump"):
+	if not stopmove and not is_on_jumper and Input.is_action_pressed("jump"):
 		if is_on_ground():
+			if jump_charge<max_jc:
+				jump_charge += 0.02
 			if last_direction == -1.0:
-				lr_anim=false
-				$AnimatedSprite2D.play("jump_left")
-				$AnimatedSprite2D.set_frame_and_progress(3,0)
-				if jump_charge<max_jc:
-					jump_charge += 0.02
+				$AnimatedSprite2D.play("charge_left")
 			else:
-				lr_anim=false
-				$AnimatedSprite2D.play("jump_right")
-				$AnimatedSprite2D.set_frame_and_progress(3,0)
-				if jump_charge<max_jc:
-					jump_charge += 0.02
+				$AnimatedSprite2D.play("charge_right")
 					
-	if not stopmove and not is_on_jumper and Input.is_action_just_pressed("jump") and not is_on_ground() and air_jump_done == 0:
+	if not stopmove and not is_on_jumper and Input.is_action_just_pressed("jump") and not is_on_ground() and air_jump_done < MAX_AIR_JUMPS:
 		velocity.y = get_jump_velocity()
 		air_jump_done += 1
 		$JumpSound.play()
 	
 	if Input.is_action_just_released("jump"):
 		if is_on_ground():
-			if last_direction == -1.0:
-				lr_anim=false
-				$AnimatedSprite2D.play("jump_left")
-			else:
-				lr_anim=false
-				$AnimatedSprite2D.play("jump_right")
+			print(last_direction)
+			if last_direction ==1 or $AnimatedSprite2D.animation=="charge_right":
+				$AnimatedSprite2D.play("onair_right")
+			if last_direction ==-1 or $AnimatedSprite2D.animation=="charge_left":
+				$AnimatedSprite2D.play("onair_left")
+			velocity.y = get_jump_velocity() * jump_charge
+			jump_charge = min_jc
+			$JumpSound.play()
+			
+			
 	
 	if position.y > 900 or position.y < -900:
 		GameData.last_death_cause = "fell_out"
@@ -85,16 +84,31 @@ func _physics_process(delta: float) -> void:
 
 	# Handle movement A-D
 	var direction := Input.get_axis("move_left", "move_right")
-	if is_dead or stopmove: 
-		direction=0
-	if last_direction != direction and lr_anim:
-		if direction == -1:
-			$AnimatedSprite2D.play("left")
-		if direction == 1:
-			$AnimatedSprite2D.play("right")
-		if direction ==0:
-			$AnimatedSprite2D.play("idle")	
-	last_direction=direction
+	
+	if last_direction != direction and not stopmove:
+		if is_on_ground() and jump_charge == min_jc: # jumpcharge = min meaning not charging -> use move animation
+			if direction == -1:
+				$AnimatedSprite2D.play("left")
+			if direction == 1:
+				$AnimatedSprite2D.play("right")
+			if direction ==0:
+				$AnimatedSprite2D.play("idle")	
+		if not is_on_ground():
+			if direction == -1:
+				$AnimatedSprite2D.play("onair_left")
+			if direction == 1:
+				$AnimatedSprite2D.play("onair_right")
+			if direction ==0:
+				$AnimatedSprite2D.play("idle")	
+	
+	if direction == 0 or direction == null:
+		if jump_charge==min_jc:
+			last_direction=direction
+	else:
+		last_direction=direction
+		
+	print(last_direction)
+	
 
 	#disable movement for an instant if on jumper
 	if direction and !stopmove:
@@ -106,27 +120,21 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, speed)
 
 	#animation reset after jump
-	if $AnimatedSprite2D.animation== "jump_right" and $AnimatedSprite2D.frame == 9:
+	if is_on_ground() and jump_charge==min_jc:
+		print("can move and on ground")
 		last_direction = null
-		lr_anim=true
-	if $AnimatedSprite2D.animation== "jump_left" and $AnimatedSprite2D.frame == 9:
-		last_direction = null
-		lr_anim=true
 
 	move_and_slide()
-	#press X to die, testing only
-	if Input.is_action_just_pressed("die"):
-		die()
 
-func _on_animated_sprite_2d_frame_changed():
-	if $AnimatedSprite2D.animation == "jump_right":
-		if $AnimatedSprite2D.frame == 4 and is_on_ground():
-			velocity.y = get_jump_velocity() * jump_charge
-			$JumpSound.play()
-	if $AnimatedSprite2D.animation == "jump_left":
-		if $AnimatedSprite2D.frame == 4 and is_on_ground():
-			velocity.y = get_jump_velocity() * jump_charge
-			$JumpSound.play()
+#func _on_animated_sprite_2d_frame_changed():
+	#if $AnimatedSprite2D.animation == "jump_right":
+	#	if $AnimatedSprite2D.frame == 4 and is_on_ground():
+	#		velocity.y = get_jump_velocity() * jump_charge
+	#		$JumpSound.play()
+	#if $AnimatedSprite2D.animation == "jump_left":
+	#	if $AnimatedSprite2D.frame == 4 and is_on_ground():
+	#		velocity.y = get_jump_velocity() * jump_charge
+	#		$JumpSound.play()
 
 
 func die(cause: String = "trap") -> bool:
