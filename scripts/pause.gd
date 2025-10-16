2e1175dba5a5e24d8e0b32bf2dcadfa8ff398da5
vanphuc1704 <kingbach9961@gmail.com>
2025-10-16 15:06:30 +0700

    Sound Options

--- scripts/pause.gd
+++ scripts/pause.gd
@@ -6,21 +6,32 @@ extends Control
 @onready var restart_button = $Panel/RestartButton
 @onready var menu_button = $Panel/MenuButton
 
+@onready var sound_button = $Panel/SoundButton
+
 # camera2d zoom settings
 @onready var zoom_close_button = $Panel/ZoomCloseButton
 @onready var zoom_normal_button = $Panel/ZoomNormalButton
 @onready var zoom_far_button = $Panel/ZoomFarButton
 
 var camera: Camera2D
+var audio_options
 
 func _ready():
 	pause_panel.visible = false
+	audio_options = pause_panel.get_node("AudioOptions")
+	if audio_options:
+		audio_options.visible = false
+	else:
+		push_error("AudioOptions is null!")
+	
 
 	# connect pause buttons
 	pause_button.pressed.connect(_on_pause_pressed)
 	continue_button.pressed.connect(_on_continue_pressed)
 	restart_button.pressed.connect(_on_restart_pressed)
 	menu_button.pressed.connect(_on_menu_pressed)
+	if sound_button:
+		sound_button.pressed.connect(_on_sound_button_pressed)
 
 	# connect zoom buttons
 	zoom_close_button.pressed.connect(_on_zoom_close_pressed)
@@ -33,7 +44,6 @@ func _ready():
 		camera = player.get_node("Camera2D")
 	else:
 		print("⚠️ Player or Camera2D not found!")
-
 
 # === Pause handling ===
 func open_pause_menu():
@@ -58,6 +68,8 @@ func _on_menu_pressed():
 	get_tree().paused = false
 	get_tree().change_scene_to_file("res://scenes/StartUI.tscn")
 
+func _on_sound_button_pressed():
+	audio_options.visible = !audio_options.visible
 
 # === Zoom handling ===
 func _on_zoom_close_pressed():
