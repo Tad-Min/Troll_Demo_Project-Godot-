1d2450a7de5be81900f7457abe64083466d890d9
VolDeBug <hiepvl987654321@gmail.com>
2025-10-17 18:13:06 +0700

    Lvl20_90%

---
+++ scripts/Trap/shrink_trapzone.gd
@@ -0,0 +1,38 @@
+extends Area2D
+
+@export var shuriken_scene: PackedScene
+@export var shuriken_speed: float = 200.0
+@export var shuriken_size: float = 1.0
+@export var min_scale: float = 0.0
+@export var shrink_duration: float = 10.0
+
+@export var spawner: NodePath               # 1 marker duy nhất
+@export var shoot_direction: Vector2 = Vector2.LEFT   # chỉnh trong Inspector
+
+var can_trigger: bool = true
+
+func _ready() -> void:
+	connect("body_entered", Callable(self, "_on_body_entered"))
+
+func _on_body_entered(body: Node) -> void:
+	if body.is_in_group("Player") and can_trigger:
+		var spawn_node = get_node_or_null(spawner)
+		if spawn_node:
+			_spawn_shuriken(spawn_node.global_position, shoot_direction)
+			start_cooldown()
+		else:
+			push_warning("Spawner node không tồn tại, kiểm tra lại NodePath trong Inspector.")
+
+func _spawn_shuriken(pos: Vector2, dir: Vector2) -> void:
+	if shuriken_scene:
+		var s = shuriken_scene.instantiate()
+		get_tree().current_scene.add_child(s)
+		s.global_position = pos
+		# gọi setup trong shuriken.gd
+		if s.has_method("setup"):
+			s.setup(dir, shuriken_speed,shrink_duration, shuriken_size, min_scale)
+		else:
+			push_warning("⚠️ Script shrunkriken chưa có hàm setup(dir, speed, duration, size, min_shrink)")
+
+func start_cooldown() -> void:
+	can_trigger = false
