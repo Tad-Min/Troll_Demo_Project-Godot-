0ca570ffdd42640fbef11226ce9a5b418d806855
vanphuc1704 <kingbach9961@gmail.com>
2025-11-18 16:51:03 +0700

    Add lv 26 to 36 into select level

--- scripts/UI_Scripts/select_level_3.gd
+++ scripts/UI_Scripts/select_level_3.gd
@@ -1,9 +1,34 @@
 extends Control
-@onready var btn_lv13: Button = $LevelMenu/btnlv25
+@onready var btn_lv25: Button = $LevelMenu/btnlv25
+@onready var btn_lv26: Button = $LevelMenu/btnlv26
+@onready var btn_lv27: Button = $LevelMenu/btnlv27
+@onready var btn_lv28: Button = $LevelMenu/btnlv28
+@onready var btn_lv29: Button = $LevelMenu/btnlv29
+@onready var btn_lv30: Button = $LevelMenu/btnlv30
+@onready var btn_lv31: Button = $LevelMenu/btnlv31
+@onready var btn_lv32: Button = $LevelMenu/btnlv32
+@onready var btn_lv33: Button = $LevelMenu/btnlv33
+@onready var btn_lv34: Button = $LevelMenu/btnlv34
+@onready var btn_lv35: Button = $LevelMenu/btnlv35
+@onready var btn_lv36: Button = $LevelMenu/btnlv36
 func _ready() -> void:
-	btn_lv13.pressed.connect(func(): _on_choose_level(25))
+	btn_lv25.pressed.connect(func(): _on_choose_level(25))
+	btn_lv26.pressed.connect(func(): _on_choose_level(26))
+	btn_lv27.pressed.connect(_on_any_button_pressed)
+	btn_lv28.pressed.connect(_on_any_button_pressed)
+	btn_lv29.pressed.connect(_on_any_button_pressed)
+	btn_lv30.pressed.connect(_on_any_button_pressed)
+	btn_lv31.pressed.connect(_on_any_button_pressed)
+	btn_lv32.pressed.connect(_on_any_button_pressed)
+	btn_lv33.pressed.connect(_on_any_button_pressed)
+	btn_lv34.pressed.connect(_on_any_button_pressed)
+	btn_lv35.pressed.connect(_on_any_button_pressed)
+	btn_lv36.pressed.connect(_on_any_button_pressed)
 func _on_choose_level(lv: int) -> void:
 	GameData.current_level = lv - 1
 	get_tree().change_scene_to_file("res://scenes/Level/Lv%d.tscn" % lv)
+func _on_any_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/GameSceneUI/ComingSoon.tscn")
+
 func _on_btn_back_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/GameSceneUI/SelectLevel2.tscn")
