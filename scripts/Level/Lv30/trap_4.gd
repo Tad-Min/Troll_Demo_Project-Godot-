94b275e709206d2db0263d200ea209a0a5951f15
Tad-Min <183442531+Tad-Min@users.noreply.github.com>
2025-11-19 10:03:08 +0700

    fix

--- scripts/Level/Lv30/trap_4.gd
+++ scripts/Level/Lv30/trap_4.gd
@@ -1,9 +1,9 @@
 extends Area2D
 
-func _ready() -> void:
-	connect("area_entered", Callable(self, "_area_entered"))
-	
-func _area_entered(area: Node) -> void:
+func _ready():
+	connect("area_entered", Callable(self, "_on_enter"))
+
+func _on_enter(_area: Area2D):
 	call_deferred("set_monitoring", false)
 	$"../FloorUp".isMove = true
 	$"../FloorUp".start()
