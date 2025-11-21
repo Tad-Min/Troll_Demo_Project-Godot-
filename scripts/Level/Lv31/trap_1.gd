bd98bc62bfb0359f8ac17f19913c2bec3d9526cb
Tad-Min <183442531+Tad-Min@users.noreply.github.com>
2025-11-22 00:19:07 +0700

    lv33

---
+++ scripts/Level/Lv31/trap_1.gd
@@ -0,0 +1,12 @@
+extends Area2D
+
+func _ready() -> void:
+	connect("body_entered", Callable(self, "_body_entered"))
+	
+func _body_entered(body: Node) -> void:
+	if body.is_in_group("Player"):
+		call_deferred("set_monitoring", false)
+		$"../rotateFloor".isRotate = true
+		$"../rotateFloor".start()
+		$"../Button/Area2D".monitoring = true
+		$"../Button".visible = true
---
+++ scripts/Level/Lv31/trap_2.gd
@@ -0,0 +1,12 @@
+extends Area2D
+
+func _ready() -> void:
+	connect("body_entered", Callable(self, "_body_entered"))
+	
+func _body_entered(body: Node) -> void:
+	if body.is_in_group("Player"):
+		call_deferred("set_monitoring", false)
+		$"../SpikeSimple".visible =  true
+		$"../SpikeSimple/Area2D".monitoring = true
+		
+		
