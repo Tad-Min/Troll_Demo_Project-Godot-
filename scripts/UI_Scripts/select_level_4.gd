c334630daee1c464ad64b58ca017dcdfd035fca2
Le Vu Van <levuvanbat11@gmail.com>
2025-12-06 17:10:16 +0700

    Update UI lv46 for Boss icon unlocking

--- scripts/UI_Scripts/select_level_4.gd
+++ scripts/UI_Scripts/select_level_4.gd
@@ -9,11 +9,26 @@ extends Control
 @onready var btn_lv44: Button = $LevelMenu/btnlv44
 @onready var btn_lv45: Button = $LevelMenu/btnlv45
 @onready var btn_lv46: Button = $LevelMenu/btnlv46
-@onready var btn_lv47: Button = $LevelMenu/btnlv47
-@onready var btn_lv48: Button = $LevelMenu/btnlv48
 @onready var key_label: Label = $LevelMenu/KeyLabel
 
+const LOCK_ICON = preload("res://assets/Jungle Asset Pack/parallax background/LockScreen.png")
+
+var _original_skull_icon: AnimatedTexture = null
+
 func _ready() -> void:
+	# Save original skull icon
+	if btn_lv46.icon is AnimatedTexture:
+		_original_skull_icon = btn_lv46.icon as AnimatedTexture
+	else:
+		# Create AnimatedTexture if not already set
+		var skull_texture = preload("res://assets/skull/1.png")
+		var skull_texture2 = preload("res://assets/skull/2.png")
+		_original_skull_icon = AnimatedTexture.new()
+		_original_skull_icon.frames = 2
+		_original_skull_icon.set_frame_texture(0, skull_texture)
+		_original_skull_icon.set_frame_texture(1, skull_texture2)
+		_original_skull_icon.set_frame_duration(1, 1.0)
+	
 	_update_lv46_unlock_status()
 	btn_lv37.pressed.connect(func(): _on_choose_level(37))
 	btn_lv38.pressed.connect(func(): _on_choose_level(38))
@@ -25,8 +40,7 @@ func _ready() -> void:
 	btn_lv44.pressed.connect(func(): _on_choose_level(44))
 	btn_lv45.pressed.connect(func(): _on_choose_level(45))
 	btn_lv46.pressed.connect(_on_lv46_button_pressed)
-	btn_lv47.pressed.connect(_on_any_button_pressed)
-	btn_lv48.pressed.connect(_on_any_button_pressed)
+
 func _on_choose_level(lv: int) -> void:
 	GameData.current_level = lv - 1
 	get_tree().change_scene_to_file("res://scenes/Level/Lv%d.tscn" % lv)
@@ -36,6 +50,17 @@ func _on_any_button_pressed() -> void:
 func _update_lv46_unlock_status() -> void:
 	var can_unlock = GameData.can_unlock_lv46()
 	btn_lv46.disabled = not can_unlock
+	
+	# Update icon: show skull when enabled, hide icon when disabled (button is already lock)
+	if can_unlock:
+		# Restore skull icon (AnimatedTexture)
+		if _original_skull_icon:
+			btn_lv46.icon = _original_skull_icon
+			btn_lv46.modulate = Color.WHITE  # Ensure full opacity
+	else:
+		# Hide icon since button style is already lock
+		btn_lv46.icon = null
+		btn_lv46.modulate = Color.WHITE  # Ensure full opacity
 	
 	# Update key label
 	if key_label:
